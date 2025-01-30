2f3278e0888cde76e16e4e17aed311e737a727f6
Richard Neat <rneat@hotmail.co.uk>
2025-01-30 18:18:10 +0000

    add BGG integration for game images

---
+++ BGHub.FE/BGHub.FE.Client/Models/BGGResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace BGHub.FE.Models
+{
+    public class BGGResponse
+    {
+        public Boardgames Boardgames { get; set; }
+    }
+    public class Boardgames
+    {
+        public Boardgame Boardgame { get; set; }
+    }
+    public class Boardgame
+    {
+        public string Image { get; set; }
+    }
+}
