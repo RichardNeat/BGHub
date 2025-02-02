6fe0c9c6f1732479ff62e5b3a575d5bb28a785a3
Richard Neat <rneat@hotmail.co.uk>
2025-02-02 15:17:34 +0000

    add event details integration with the backend

--- BGHub.FE/BGHub.FE/Program.cs
+++ BGHub.FE/BGHub.FE/Program.cs
@@ -17,10 +17,10 @@ builder.Services.AddCascadingValue(sp =>
         Name = "Rich",
         Games = new List<Game>
         {
-            new Game {Id=5, Name="Blood on the Clocktower", Owner=new User{ Id=1, Name="Rich"}, BGGId=240980, ImageUrl="https://cf.geekdo-images.com/HINb2nkFn5IiZxAlzQIs4g__original/img/e7izEwSmnBPiErsIF6hlWbgybBE=/0x0/filters:format(jpeg)/pic7009391.jpg"},
-            new Game {Id=4, Name="Legendary Encounters: An Alien Deck Building Game", Owner=new User{ Id=1, Name="Rich"}, BGGId=146652, ImageUrl="https://cf.geekdo-images.com/jSz_KRUxsjGYitoqx9YH1Q__original/img/nrvJtT-4tYTD0fp3Cr57SKpfMfw=/0x0/filters:format(jpeg)/pic2225180.jpg" },
-            new Game {Id=2, Name="Slay the Spire", Owner=new User{ Id=1, Name="Rich"}, BGGId=338960, ImageUrl="https://cf.geekdo-images.com/PQzVclEoOQ_wr4e1V86kxA__original/img/KXOf1hP1cIJQLabKhZulWP-e9wI=/0x0/filters:format(png)/pic8157856.png"},
-            new Game {Id=1, Name="Terraforming Mars", Owner=new User{ Id=1, Name="Rich"}, BGGId=167791, ImageUrl="https://cf.geekdo-images.com/wg9oOLcsKvDesSUdZQ4rxw__original/img/thIqWDnH9utKuoKVEUqveDixprI=/0x0/filters:format(jpeg)/pic3536616.jpg"}
+            new Game {Id=5, Name="Blood on the Clocktower", OwnerId=1, Owner=new User{ Id=1, Name="Rich"}, BGGId=240980, ImageUrl="https://cf.geekdo-images.com/HINb2nkFn5IiZxAlzQIs4g__original/img/e7izEwSmnBPiErsIF6hlWbgybBE=/0x0/filters:format(jpeg)/pic7009391.jpg"},
+            new Game {Id=4, Name="Legendary Encounters: An Alien Deck Building Game", OwnerId=1, Owner=new User{ Id=1, Name="Rich"}, BGGId=146652, ImageUrl="https://cf.geekdo-images.com/jSz_KRUxsjGYitoqx9YH1Q__original/img/nrvJtT-4tYTD0fp3Cr57SKpfMfw=/0x0/filters:format(jpeg)/pic2225180.jpg" },
+            new Game {Id=2, Name="Slay the Spire", OwnerId=1, Owner=new User{ Id=1, Name="Rich"}, BGGId=338960, ImageUrl="https://cf.geekdo-images.com/PQzVclEoOQ_wr4e1V86kxA__original/img/KXOf1hP1cIJQLabKhZulWP-e9wI=/0x0/filters:format(png)/pic8157856.png"},
+            new Game {Id=1, Name="Terraforming Mars", OwnerId=1, Owner=new User{ Id=1, Name="Rich"}, BGGId=167791, ImageUrl="https://cf.geekdo-images.com/wg9oOLcsKvDesSUdZQ4rxw__original/img/thIqWDnH9utKuoKVEUqveDixprI=/0x0/filters:format(jpeg)/pic3536616.jpg"}
         },
         BGGUsername = "Gandolfini"
     };
